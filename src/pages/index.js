84526cecf2a6db1e0b919fd6831920dcc31668e0
Luiz Venturote <luiz@imaginemage.com>
2019-07-29 00:01:00 -0400

    Fix text

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
         <Image />
       </div>
       <p>Hi, I'm Luiz Venturote.</p>
-      <p>I'm a UI/UX designer and front-end developer at <a href='https://www.imaginationmedia.com/' rel="noopener">Imagination Media</a>, a Magento Partner agency based in Florida. I'm responsible for e-commerce development using Magento, Sales Force Commerce Cloud and other cool platforms with a recent focus on modern Javascript.</p>
+      <p>I'm a UI/UX designer and front-end developer at <a href='https://www.imaginationmedia.com/' rel="noopener">Imagination Media</a>, a Magento Partner agency based in Florida. I'm responsible for e-commerce development using Magento, Salesforce Commerce Cloud and other cool platforms with a recent focus on modern Javascript.</p>
       <p>I live in a little town in Brazil where I organize a <a href='https://meetup.com/dev-colatina/' rel="noopener">meetup about software development</a>. I have three <a href='https://u.magento.com/certification/directory/dev/2261168/' rel="noopener">Magento certifications</a> and graduated with a degree in Information Systems at <a href='http://ifes.edu.br/' rel="noopener">Ifes</a>.</p>
       <p>I enjoy music, learning foreign languages and Bible.</p>
     </div>
