488a0fe1e83a7b2f09d4628c30191cb96cc3a2c2
Luiz Venturote <luiz@imaginationmedia.com>
2023-06-05 12:10:46 -0300

    Update Portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,2 +1,19 @@
- <script>window.location = 'https://chat.whatsapp.com/GppWvblOS4xAgMoEBcuaLd';</script>
-    
+import React from "react"
+import { useEffect } from "react"
+import Layout from "../components/layout"
+
+export default ({data}) => {
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        // 👇️ redirects to an external URL
+        window.location.replace('https://venturote.notion.site/Luiz-Venturote-5329276ac505419e80b60b385749d9ab?pvs=4');
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <Layout>Will redirect in 3 seconds...</Layout>
+    )
+}
