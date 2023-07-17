4050a871c537fac66791060cc2fb5bc604307d8c
Luiz Venturote <luiz@imaginationmedia.com>
2023-07-17 07:21:53 -0300

    Add Payoneer verification

---
+++ src/pages/payonner.js
@@ -0,0 +1,22 @@
+import React from "react"
+import TalkSection from "../components/talk-section"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import Styles from '../components/styles/page.module.css'
+
+export default ({data}) => {
+  return (
+  <Layout>
+
+    <SEO title="Talks" />
+
+    <h1 className={Styles.pageTitle}>Verificação do Payoneer</h1>
+    <p>Meu nome: Luiz Alberto Binda Venturote</p>
+    <p>Nome da empresa: Venturote Tecnologia LTDA</p>
+    <p>E-mail: luiz@venturote.com</p>
+    <p>Este site pertence e é operado por Luiz Alberto Binda Venturote.</p>
+
+  </Layout>
+)}
